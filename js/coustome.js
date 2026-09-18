0d9acf47698d4fde5835462e65ad0bcfd0d0f84b
Rohit sonagara <rohitsonagar25@gmail.com>
2026-09-18 17:08:41 +0530

    Add
    Add

---
+++ js/coustome.js
@@ -0,0 +1,28 @@
+
+ var owl = $('.banner-section .owl-carousel');
+ owl.owlCarousel({
+    loop:true,
+    nav:true,
+    autoplay:true,
+    items:1,
+ animateOut: 'animate__fadeOut',
+    animateIn: 'animate__fadeIn',
+    
+ });
+
+// $(document).ready(function(){
+// document.addEventListener(
+//                 "DOMContentLoaded", () => {
+//                     new Mmenu( "#menu", {
+//                        "offCanvas": {
+//                           "position": "left"
+//                        },
+//                        "theme": "dark"
+//                     });
+//                 }
+//             );
+// })
+
+
+
+
